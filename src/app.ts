4ca432b3bbde98235b3ac7d16a5233310b00a40f
unknown <mydulcse62.niter@gmail.com>
2025-04-21 21:23:27 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import {Application, Request,Response} from 'express'
+import express from'express'
+import cors from 'cors'
+const app:Application = express();
+app.use(cors());
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+
+})
+
+export default app;
